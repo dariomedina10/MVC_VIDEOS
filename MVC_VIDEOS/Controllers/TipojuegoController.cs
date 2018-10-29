f0a8417e1382719a19a2a84ff109d6597bd1848f
Darío Medina <dariomedina10@gmail.com>
2018-10-29 13:15:14 -0400

    crud tipo_juego

--- MVC_VIDEOS/Controllers/TipojuegoController.cs
+++ MVC_VIDEOS/Controllers/TipojuegoController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MVC_VIDEOS;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,15 +12,31 @@ namespace MVC_VIDEOS.Controllers
     public class TipojuegoController : Controller
     {
         // GET: Tipojuego
+        private bd_video_juegosEntities db = new bd_video_juegosEntities();
         public ActionResult Index()
         {
+            ViewBag.tipojuego = db.tipo_juego.ToList();
+
             return View();
         }
 
         // GET: Tipojuego/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var tipojuego = db.tipo_juego.Find(id);
+            if (tipojuego == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(tipojuego);
+
+
         }
 
         // GET: Tipojuego/Create
@@ -28,62 +47,106 @@ namespace MVC_VIDEOS.Controllers
 
         // POST: Tipojuego/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(tipo_juego tipo_Juego)
         {
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.tipo_juego.Add(tipo_Juego);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(tipo_Juego);
             }
             catch
             {
-                return View();
+                return View(tipo_Juego);
             }
         }
 
         // GET: Tipojuego/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var tipojuego = db.tipo_juego.Find(id);
+            if (tipojuego == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(tipojuego);
+
         }
 
         // POST: Tipojuego/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(tipo_juego tipo_Juego)
         {
             try
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Entry(tipo_Juego).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(tipo_Juego);
             }
             catch
             {
-                return View();
+                return View(tipo_Juego);
             }
         }
 
         // GET: Tipojuego/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var tipojuego = db.tipo_juego.Find(id);
+            if (tipojuego == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(tipojuego);
+
+
         }
 
         // POST: Tipojuego/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int? id, tipo_juego tipo_juego)
         {
             try
             {
-                // TODO: Add delete logic here
+                if (ModelState.IsValid)
+                {
+                    tipo_juego = db.tipo_juego.Find(id);
+                    if (tipo_juego == null)
+                    {
+                        return HttpNotFound();
+                    }
 
-                return RedirectToAction("Index");
+                    db.tipo_juego.Remove(tipo_juego);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(tipo_juego);
             }
             catch
             {
-                return View();
+                return View(tipo_juego);
             }
+
         }
     }
 }
